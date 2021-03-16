9365179345c258b6a5dcc50b38867d983be5abf9
BartoszGamza <bartosz.gamza12@gmail.com>
2021-03-16 22:31:52 +0100

    generate posts links

--- index.js
+++ index.js
@@ -1,35 +1,50 @@
-const express = require('express')
-const request = require('request')
+// const express = require('express')
+const axios = require('axios')
 const cheerio = require('cheerio')
 const download = require('node-image-downloader')
 
-const scrapImages = (url) => {
-    request(url, (err, res, html) => {
-        const $ = cheerio.load(html)
-
-        const header = $('#Header1_headerimg').attr('src')
-
-        console.log('header link', header)
-
-        download({
-            imgs: [{
-                uri: header,
-                name: 'logo'
-            }],
-            dest: './downloads'
-        }).then(() => console.log('done')).catch(err => console.log(err))
+function extractLinks ($, objects, requireBlogPage = false) {
+    const targetArray = []
+    objects.each((_, element) => {
+        const href = $(element).attr('href')
+        if (href && href.includes('minimalb.art')) {
+            if (requireBlogPage) {
+                if (href.endsWith('.html')) {
+                    targetArray.push(href)
+                }
+            } else {
+                targetArray.push(href)
+            }
+        }
     })
+    return targetArray
+}
 
+async function getLinksBySelector (url, selector, blogPages = false) {
+    return axios.get(url).then(response => {
+        const $ = cheerio.load(response.data)
+        const expandableObjects = $(selector)
+        return extractLinks($, expandableObjects, blogPages)
+    }) 
+}
+
+async function getBlogPages (url) {
 
 }
 
-const app = express()
+const url = 'http://www.minimalb.art/'
+const collapisbleObjectSelector = '.post-count-link'
+const linkSelector = 'a'
 
-app.get('/', (req, res) => {
-    const url = 'http://www.minimalb.art/'
+getLinksBySelector(url, collapisbleObjectSelector).then(historicalLinks => {
+    // console.log(historicalLinks)
+    const result = async () => Promise.all(historicalLinks.map(async link => {
+        return getLinksBySelector(link, linkSelector, true)
+    }))
 
-    scrapImages(url)
+    result().then(res => {
+        const flattened = [].concat.apply([], res)
+        const uniq = [...new Set(flattened)]
+        console.log(uniq)
+    })
 })
-
-app.listen(5000)
-
